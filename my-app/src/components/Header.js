4bfc5414c5ab788c56872beebf6b0dc3cf70428b
Haleigh Rzonca <hgielah@gmail.com>
2023-01-17 17:02:46 -0800

    Header completed, Working on navigation and resume js

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,2 +1,47 @@
-//import React from 'react';
+import React from 'react';
+
+function Header({ currentPage, handlePageChange }) {
+    return (
+        <ul className="nav">
+            <li className="nav-content">
+                <a
+                    href="#about"
+                    onClick={() => handlePageChange('About')}
+                    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                >
+                    About Me
+                </a>
+            </li>
+            <li className="nav-content">
+                <a
+                    href="#Portfolio"
+                    onClick={() => handlePageChange('Portfolio')}
+                    className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                >
+                    Portfolio
+                </a>
+            </li>
+            <li className="nav-content">
+                <a
+                    href="#Contact"
+                    onClick={() => handlePageChange('Contact')}
+                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                    Contact
+                </a>
+            </li>
+            <li className="nav-content">
+                <a
+                    href="#Resume"
+                    onClick={() => handlePageChange('Resume')}
+                    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                >
+                    Contact
+                </a>
+            </li>
+        </ul>
+    );
+}
+
+export default Header;
 
--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -1,46 +1,15 @@
 import React from 'react';
+import {Navlink} from "react-router-dom";
 
-function Navigation({ currentPage, handlePageChange }) {
+function Navigation(props) {
     return (
-        <ul className="nav">
-            <li className="nav-content">
-                <a
-                    href="#about"
-                    onClick={() => handlePageChange('About')}
-                    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-                >
-                    About Me
-                </a>
-            </li>
-            <li className="nav-content">
-                <a
-                    href="#Portfolio"
-                    onClick={() => handlePageChange('Portfolio')}
-                    className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-                >
-                    Portfolio
-                </a>
-            </li>
-            <li className="nav-content">
-                <a
-                    href="#Contact"
-                    onClick={() => handlePageChange('Contact')}
-                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                >
-                    Contact
-                </a>
-            </li>
-            <li className="nav-content">
-                <a
-                    href="#Resume"
-                    onClick={() => handlePageChange('Resume')}
-                    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-                >
-                    Contact
-                </a>
-            </li>
-        </ul>
-    );
+        <div className='row navigation' id='navigation'>
+            <Navlink to="/about">About</Navlink>
+            <Navlink to="/project">Project</Navlink>
+            <Navlink to="/contact">Contact</Navlink>
+            <Navlink to="/resume">Resume</Navlink>
+        </div>
+    )
 }
 
 export default Navigation;
